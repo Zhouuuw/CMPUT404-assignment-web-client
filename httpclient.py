0dfa16b8f32da407d2295b5e4a4c26f884899596
Zhouuuw <ninawen2016@gmail.com>
2020-02-07 17:02:26 -0700

    as

--- httpclient.py
+++ httpclient.py
@@ -24,6 +24,13 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+class Error(Exception):
+    def __init__(self,ErrorInfo):
+        super().__init__(self) 
+        self.errorinfo=ErrorInfo
+    def __str__(self):
+        return self.errorinfo
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,7 +40,40 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        count = 0
+        for s in url:
+            if s == ":":
+                count += 1
+
+        if count == 2:
+            url_list = url.split("/")
+            if len(url_list) < 3:
+                raise Exception 
+        
+            temp = url_list[2].split(":")
+            if len(temp) != 2:
+                    raise Exception
+            addr = "/"
+            if len(url_list) >3 :
+                for s in  url_list[3:]:
+                    addr += s + "/"
+            temp[1] = int(temp[1])
+            return (temp[0],temp[1],addr)
+        else:
+            # use socket to find host and port
+            # https://blog.csdn.net/keep_lcm/article/details/81008801
+            
+            url = url.strip("/")
+            print(url)
+            print(url[6:])
+            lst = socket.getaddrinfo(url[7:],"http")
+            print(lst)
+            print("--------urg url is ---------")
+            print(lst[0])
+            print(lst[0][4])
+            return (lst[0][4][0],int(lst[0][4][1]),"")
+        
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,7 +81,10 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        l = data.split(" ")
+        print("attention")
+        print(l)
+        return int(l[1])
 
     def get_headers(self,data):
         return None
@@ -49,6 +92,17 @@ class HTTPClient(object):
     def get_body(self, data):
         return None
     
+    def strip_response(self, response):
+        lst = response.split("\r\n")
+        #print("--------response data is ---------")
+        #print(lst)
+        code = self.get_code(lst[0])
+        if lst[-1] == "":
+            body = lst[-2]
+        else:
+            body = lst[-1]
+        return code,body
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
@@ -70,11 +124,38 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        host, port, addr = self.get_host_port(url)
+        self.connect(host,port)
+        get_info = "GET "+addr+ " HTTP/1.1\r\nHost: "+host+"\r\n\r\n"
+        #print("--------get info is ---------")
+        #print(get_info)
+        self.sendall(get_info)
+        response = self.recvall(self.socket)
+        #print("--------response data is ---------")
+        #print(response)
+        code,body = self.strip_response(response)
+        
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        host, port, addr = self.get_host_port(url)
+        self.connect(host,port)
+        if args == None:
+            get_info = "POST "+addr+ " HTTP/1.1\r\nHost: "+host+"\r\n"+"Content-length : 0\r\n\r\n"
+        else:
+            get_info = "POST "+addr+ " HTTP/1.1\r\nHost: "+host+"\r\n"+"Content-length : "+str(len(args))+"\r\n"+args+"\r\n\r\n"
+        #print("--------get info is ---------")
+        #print(get_info)
+        self.sendall(get_info)
+        response = self.recvall(self.socket)
+        print("--------response data is ---------")
+        print(response)
+        code,body = self.strip_response(response)
+        
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
